a92a26614b296698f1f17613a93063d2101d86ae
hsiaofongw <hsiaofong.w@gmail.com>
2021-04-09 19:27:11 +0800

    feat: updated example data

--- src/app/phones-data.ts
+++ src/app/phones-data.ts
@@ -6,8 +6,8 @@ interface PhoneInventory extends Phone {
 
 // 记录在售手机的初始库存情况
 export const phones: PhoneInventory[] = [
-    { id: 0, modelName: "phone1", description: "desc1", inStockQuantity: 1, price: 100 },
-    { id: 1, modelName: "phone2", description: "desc2", inStockQuantity: 10, price: 200 },
-    { id: 2, modelName: "phone3", description: "desc3", inStockQuantity: 20, price: 300 },
-    { id: 3, modelName: "phone4", description: "desc4", inStockQuantity: 100, price: 400 }
+    { id: 0, modelName: "PhoneX", description: "新的 Phone, 比更新还更新。", inStockQuantity: 7, price: 999 },
+    { id: 1, modelName: "PhoneS", description: "小的 Phone, 玲珑小巧。", inStockQuantity: 10, price: 1999 },
+    { id: 2, modelName: "PhoneLLLL", description: "充满创新，充满伟大的创新。", inStockQuantity: 9, price: 2999 },
+    { id: 3, modelName: "Phone12", description: "快来体验最新技术。", inStockQuantity: 16, price: 3999 }
 ];
